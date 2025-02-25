643c3da26ff7f0f8724d2d97811bfa9b8c4347fa
wiiiolettaaaaa <160237848+wiiiolettaaaaa@users.noreply.github.com>
2025-02-25 23:29:15 +0200

    implement http sever

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request:", r.Method, r.URL.Path)
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/time", http.StatusFound)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/time", timeHandler)
+
+	port := ":8795"
+	log.Println("Starting server on http://localhost" + port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal("Server failed:", err)
+	}
 }
