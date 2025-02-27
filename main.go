9c9e2165320db5bdabef0b82743c551c65917471
ProMKQ <Maximik128@gmail.com>
2025-02-27 14:55:57 +0200

    Code cleanup

--- main.go
+++ main.go
@@ -11,21 +11,21 @@ type TimeResponse struct {
 	Time string `json:"time"`
 }
 
-func timeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received request:", r.Method, r.URL.Path)
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+func timeHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Received request:", request.Method, request.URL.Path)
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Only GET method is allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		log.Println("Could not encode response:", err)
+	writer.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(writer).Encode(response); err != nil {
+		log.Fatal("Could not encode response:", err)
 	}
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/time", http.StatusFound)
+func rootHandler(writer http.ResponseWriter, request *http.Request) {
+	http.Redirect(writer, request, "/time", http.StatusFound)
 }
 
 func main() {
